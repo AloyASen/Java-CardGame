4232db523855b8704858073d55617464b2d19007
Aditya Sen lab <aloysen951@gmail.com>
2020-08-21 05:23:19 +0530

    refactoring simple card game to support multisize game player accomodations --- easily

--- src/main/java/hello/SimpleCardGame.java
+++ src/main/java/hello/SimpleCardGame.java
@@ -1,27 +1,33 @@
 package hello;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ListIterator;
+
 // this is the game
 public class SimpleCardGame {
 
    //the deck of 52 cards in this game
    Deck deck = new Deck();
    
-   // materialize the 4 players 
-   Player plyr1;
-   Player plyr2;
-   Player plyr3;
-   Player plyr4;
-   // check the game level -> initlially 0
+   // materialize the 4 players NOTE:: also extensible
+   List<Player> Plyrs =new ArrayList<>();
+   //memcached copy
+    Player[] copyPlayers ;
+   // check the game level -> initially 0
    int _isOn=0;
    // should the Game (Context) cashout and contract to exist!?
    boolean isEnded = false;
-   // while creating the game iintialize the players and make the deck 
+   // while creating the game initialize the players and make the deck
     public SimpleCardGame(Player plyr1, Player plyr2, Player plyr3, Player plyr4){
-        // initialize the three players
-        this.plyr1= plyr1;
-        this.plyr2= plyr2;
-        this.plyr3= plyr3;
-        this.plyr4= plyr4;
+        // initialize the four players
+        Plyrs.add(plyr1);
+        Plyrs.add(plyr2);
+        Plyrs.add(plyr3);
+        Plyrs.add(plyr4);
+        //initialize tihe copy players to for this game object
+        copyPlayers = new Player[Plyrs.size()];
+        Plyrs.toArray(copyPlayers);
        deck.init();// shuffles the deck as a shifter function // other processes can also be taken
 
     }
@@ -45,17 +51,14 @@ public class SimpleCardGame {
         PlayingState gameState =new PlayingState();
       _isOn =1; 
       // draw three cards for each player
-        plyr1.setCards(deck.draw3cards());
-        plyr1.setState(gameState);
 
-        plyr2.setCards(deck.draw3cards());
-        plyr2.setState(gameState);
+        for (Player pie:
+             copyPlayers) {
+            pie.setCards(deck.draw3cards());
+            pie.setState(gameState);
 
-        plyr3.setCards(deck.draw3cards());
-        plyr3.setState(gameState);
+        }
 
-        plyr4.setCards(deck.draw3cards());
-        plyr4.setState(gameState);
 
         System.out.println("all the 4 players get the initial set of cards :: 3 each");
       // compare them to check tie / end state
@@ -64,10 +67,12 @@ public class SimpleCardGame {
     public int  step(){
       _isOn +=1;
       // all the players draw a new card
-        plyr1.drawAnotherCard(deck.drawCard());
-        plyr2.drawAnotherCard(deck.drawCard());
-        plyr3.drawAnotherCard(deck.drawCard());
-        plyr4.drawAnotherCard(deck.drawCard());
+        for (Player pie:
+             copyPlayers) {
+            pie.drawAnotherCard(deck.drawCard());
+        }
+        // compare them to check tie / end state
+
         return 0;
     }
     public void end() {
